b9d0a608c27a1f98600c12b980110f4101fa4cb7
Fernandez Canosa <afernandezcanosa@anl.gov>
2019-08-01 13:16:42 -0500

    Added pedal models of the three vehicles

---
+++ pedal_models/pedal_vs_speed_accel.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+
+class PedalModel(object):
+    
+    def __init__(self, car):
+        
+        self.car = car
+        
+        if self.car == 'mazda_cx9':
+            self.coeffs = [-2.15112766e+00,
+                           1.59435249e+00, 2.54082287e+01,
+                           -5.65619082e-02, -4.96890927e-01, -1.04503861e+01,
+                           7.97139394e-04, 2.55779716e-02, 2.12937717e-01, 1.64147857e+00]
+        elif self.car == 'ford_f150':
+            self.coeffs = [2.01805436e-01,
+                           6.02884579e-01, 6.36548308e+00,
+                          -1.67426622e-02, 1.04169336e+00, -2.43369234e-01,
+                           4.63490229e-04, -9.14509267e-03, -2.10704875e-01, 1.55521523e-01]
+        elif self.car == 'ford_fusion':
+            self.coeffs = [-1.58738238e+00,
+                           1.15274390e+00, 1.31335620e+01,
+                          -3.63003606e-02, 1.12372052e-01, -5.05241104e+00,
+                           6.23982212e-04, 2.53013196e-02, 3.42877674e-01, 7.16712738e-01]        
+        else:
+            raise ValueError("Please introduce a valid vehicle")  
+            
+    def pedal_per(self, speed_mps, accel_mps2):
+        
+        """ Polynomial model of the percentage of acceleration pedal as a
+        function of speed and acceleration of the vehicle """
+        
+        accel_pedal = (self.coeffs[0] +
+                       self.coeffs[1]*speed_mps + self.coeffs[2]*accel_mps2 +
+                       self.coeffs[3]*speed_mps**2 + self.coeffs[4]*speed_mps*accel_mps2 + self.coeffs[5]*accel_mps2**2 + 
+                       self.coeffs[6]*speed_mps**3 + self.coeffs[7]*speed_mps**2*accel_mps2 + self.coeffs[8]*speed_mps*accel_mps2**2 +
+                       self.coeffs[9]*accel_mps2**3)
+        
+        if accel_pedal < 0:
+            accel_pedal = 0
+        if accel_pedal > 100:
+            accel_pedal = 100
+        
+        return accel_pedal
+            
+            
+
+if __name__ == "__main__":
+    
+    mph_to_mps = 0.44704
+    
+    cars = ['mazda_cx9', 'ford_f150', 'ford_fusion']
+    pedals = []
+    
+    for car in cars:
+        pedals.append(PedalModel(car = car))
+        
+    speed_mps = 70*mph_to_mps
+    accel_mps2 = 0.2
+    for pedal in pedals:
+        per = pedal.pedal_per(speed_mps, accel_mps2)
+        
+        print('v = %.2f m/s, a = %.2f m/s^2, car = %s pedal_per = %.2f' %(speed_mps, accel_mps2, pedal.car, per))
+        
+ 
+
+
