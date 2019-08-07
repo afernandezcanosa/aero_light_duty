9e4cbbe85cdfe753aeb809d09eaa4b97770e316c
unknown <ladmin@CSI350898.es.anl.gov>
2019-08-07 11:50:05 -0500

    Minor detail to check voltages

--- pedal_models/check_voltage_models_cars.py
+++ pedal_models/check_voltage_models_cars.py
@@ -7,6 +7,8 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_daq as daq
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import cantools
 import nidaqmx
 import plotly.graph_objs as go
@@ -20,6 +22,7 @@ from datetime import datetime
 
 # Import local libraries
 from pedal_vs_speed_accel import PedalModel as PM
+from resources import get_nidaqmx_dev_name
 
 
 # Create the app and assign excepctions
@@ -48,16 +51,17 @@ base_path = dirname(abspath(__file__))
 parent_path = abspath(join(base_path, os.pardir))
 DBC_FILE = None
 VOLTAGE_MODEL = None
+DEV = get_nidaqmx_dev_name()
 
 
 # Layout of the app
-app.layout = html.Div([ 
-        
+app.layout = html.Div([
+
     html.Link(
         rel='stylesheet',
         href='/static/stylesheet.css'
     ),
-            
+
     dcc.Interval(
         id = 'refresh',
         interval = 100,
@@ -67,7 +71,7 @@ app.layout = html.Div([
     # Aux to store the car model
     html.Div(id='store_car'),
 
-    html.Div([       
+    html.Div([
         html.Div([
             html.Label('CAR MODEL:',
                        style = {
@@ -77,9 +81,9 @@ app.layout = html.Div([
                            'position': 'relative',
                            'padding-top': 3
                        }
-            )    
+            )
         ], className = 'three columns'),
-        
+
         html.Div([
             dcc.Dropdown(
                 id='car_dropdown',
@@ -90,11 +94,11 @@ app.layout = html.Div([
                 ],
                 style = {'width': 900},
                 value='mazda_cx9'
-                ),   
-        ], className = 'nine columns'),           
-    ], className = 'row', style={'padding': 20}),           
-         
-    html.Div([       
+                ),
+        ], className = 'nine columns'),
+    ], className = 'row', style={'padding': 20}),
+
+    html.Div([
         html.Div([
             html.Label('PEDAL INPUT:',
                        style = {
@@ -104,9 +108,9 @@ app.layout = html.Div([
                            'position': 'relative',
                            'padding-top': 3
                        }
-            )    
+            )
         ], className = 'three columns'),
-        
+
         html.Div([
             daq.Slider(
                 id = 'pedal_input',
@@ -117,16 +121,16 @@ app.layout = html.Div([
                 size = 900,
                 updatemode = 'drag',
                 marks={i: '{}'.format(i) for i in range(0,101,10)},
-            )   
-        ], className = 'nine columns'),           
+            )
+        ], className = 'nine columns'),
     ], className = 'row', style={'padding': 20}),
-        
+
     html.Div([
         dcc.Graph(id='live_update_graph'),
-    ], className = 'row', style={'padding': 20})           
+    ], className = 'row', style={'padding': 20})
 ])
-    
-    
+
+
 @app.callback(
      Output('store_car', 'value'),
     [Input('car_dropdown', 'value')])
@@ -141,57 +145,57 @@ def read_can(car_name):
 
     DBC_FILE = cantools.database.load_file(car)
     VOLTAGE_MODEL = PM(car_name)
-    
-    
+
+
 @app.callback(
      Output('live_update_graph', 'figure'),
     [Input('refresh', 'n_intervals'),
 	 Input('pedal_input', 'value'),
     Input('car_dropdown', 'value')])
 def real_gas(n, pedal_input, car):
-    
+
     global DBC_FILE, VOLTAGE_MODEL, PANDA
-    
+
     v0, v1 = VOLTAGE_MODEL.voltage_from_pedal(pedal_input)
-    
+
     real_pedal = 0
-    
+
     can_recv = PANDA.can_recv()
     with nidaqmx.Task() as task:
-        task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
-        task.ao_channels.add_ao_voltage_chan("Dev1/ao1")
+        task.ao_channels.add_ao_voltage_chan(DEV + "/ao0")
+        task.ao_channels.add_ao_voltage_chan(DEV + "/ao1")
         task.write([v0, v1], auto_start=True)
-                
+
     if car == 'mazda_cx9':
-        for address, _, dat in can_recv:
+        for address, _, dat, _ in can_recv:
             if address == 0x167:
                 msg = DBC_FILE.decode_message(address, dat)
-                real_pedal = msg['Pedal_accel_pos_CAN_per_'] 
-                break   
-            
+                real_pedal = msg['Pedal_accel_pos_CAN_per_']
+                break
+
     elif car == 'ford_f150':
-        for address, _, dat in can_recv:
+        for address, _, dat, _ in can_recv:
             if address == 0x204:
                 msg = DBC_FILE.decode_message(address, dat)
-                real_pedal = msg['Pedal_accel_pos_CAN_per_'] 
-                break           
-        
+                real_pedal = msg['Pedal_accel_pos_CAN_per_']
+                break
+
     elif car == 'ford_fusion':
-        for address, _, dat in can_recv:
+        for address, _, dat, _ in can_recv:
             if address == 0x201:
                 msg = DBC_FILE.decode_message(address, dat)
-                real_pedal = msg['Pedal_accel_pos_CAN_per_'] 
-                break   
-                     
+                real_pedal = msg['Pedal_accel_pos_CAN_per_']
+                break
+
     X.append(datetime.now())
     Y0.append(pedal_input)
     Y1.append(real_pedal)
-    
+
     if len(X) >= 100:
         X.pop(0)
         Y0.pop(0)
         Y1.pop(0)
-        
+
     TRACES0 = go.Scatter(
         x = X,
         y = Y0,
@@ -203,11 +207,11 @@ def real_gas(n, pedal_input, car):
         y = Y1,
         mode = 'lines+markers',
         name = 'real_pedal'
-	)        
+	)
     return {
 		'data': [TRACES0, TRACES1]
-	}    
-    
+	}
+
 
 # Run the app in local server
 if __name__ == '__main__':
