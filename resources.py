83bdfe1b55fba049d7e585c976404ddf22243129
Fernandez Canosa <afernandezcanosa@anl.gov>
2019-08-14 10:52:12 -0500

    Added panda ports ids to allow the system to work properly (F150)

--- resources.py
+++ resources.py
@@ -9,6 +9,31 @@ def get_nidaqmx_dev_name():
     device = system.devices[0]
     return device.name
 
+def get_panda_id(car_name, option):
+    if car_name == 'mazda_cx9':
+        if option == 'recv':
+            return '53002c000c51363338383037'
+        elif option == 'send':
+            return '240050000c51363338383037'
+        else:
+            raise ValueError("Please introduce a valid option: recv - send")
+    elif car_name == 'ford_f150':
+        if option == 'recv':
+            return '360024000c51363338383037'
+        elif option == 'send':
+            return '26003f000651363038363036'
+        else:
+            raise ValueError("Please introduce a valid option: recv - send")
+    elif car_name == 'ford_fusion':
+        if option == 'recv':
+            return None
+        elif option == 'send':
+            return None
+        else:
+            raise ValueError("Please introduce a valid option: recv - send")
+    else:
+        raise ValueError("Please introduce a valid vehicle")
+        
 
 if __name__ == "__main__":
     device = get_nidaqmx_dev_name()
